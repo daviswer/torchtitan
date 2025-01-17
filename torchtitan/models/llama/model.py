86b4e77e53905e082ed5f134540323f3341a0185
Davis Wertheimer <dww78@cornell.edu>
2025-01-17 17:26:33 -0500

    diag print test

--- torchtitan/models/llama/model.py
+++ torchtitan/models/llama/model.py
@@ -262,7 +262,8 @@ class Attention(nn.Module):
             affinity = k_.matmul(kt).relu().clamp(min=0,max=1)
             print("Pre", i, affinity.min().item(), affinity.max().item())
             affinity = torch.log1p(affinity.neg().add(1e-6))  # b h c l
-            print("Affinity 1", i, affinity.std().item())
+            print("Affinity 1", i, affinity.std().item(), 
+                  torch.log1p(torch.rand(100, device=k_.device, dtype=k_.dtype).neg().add(1e-6)).std().item())
             affinity = affinity.masked_fill(torch.ones_like(affinity).tril(i*c).bool(), 0)
             affinity = affinity.cumsum(3).exp().masked_fill(torch.ones_like(affinity).tril(i*c-1).bool(), 0)
             score = k_.matmul(xq.transpose(-2,-1))  # b h c l
