074f0e10a258aa4f17f9bacac6505d7383d2804a
Davis Wertheimer <dww78@cornell.edu>
2025-01-29 12:58:40 -0500

    Diag print

--- torchtitan/models/llama/model.py
+++ torchtitan/models/llama/model.py
@@ -307,6 +307,7 @@ class Attention(nn.Module):
             0, 3, 1, 2, 4
         ).contiguous()  # (bs, seqlen, n_local_heads, n_rep, head_dim)
         output = output.view(bs, seqlen, -1)
+        print(output.std())
         return self.wo(output)
 
 
