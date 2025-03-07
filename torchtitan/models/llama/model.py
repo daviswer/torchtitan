fedbda177fdb43f9f16ebc48d3d9c69247bd504a
Davis Wertheimer <dww78@cornell.edu>
2025-03-06 19:14:04 -0500

    Attempt statics

--- torchtitan/models/llama/model.py
+++ torchtitan/models/llama/model.py
@@ -11,6 +11,7 @@
 from dataclasses import dataclass
 from typing import Optional, Tuple
 
+import math
 import torch
 import torch.nn.functional as F
 from torch import nn
@@ -198,15 +199,21 @@ class Attention(nn.Module):
         self.wo = nn.Linear(
             model_args.n_heads * self.head_dim, model_args.dim, bias=False
         )
+        self.wstatic = nn.Linear(model_args.dim, 2*self.n_kv_heads, bias=True)
+        self.wstatic.bias._no_weight_decay = True
         # self.gn = build_norm(
         #     model_args.norm_type, dim=self.head_dim, eps=model_args.norm_eps
         # )
         # self.sinks = nn.Parameter(torch.empty(2, self.n_kv_heads, self.head_dim, self.head_dim))
 
     def init_weights(self, init_std: float):
-        for linear in (self.wq, self.wk, self.wv):
+        for linear in (self.wq, self.wk, self.wv, self.wstatic):
             nn.init.trunc_normal_(linear.weight, mean=0.0, std=0.02)
         nn.init.trunc_normal_(self.wo.weight, mean=0.0, std=init_std)
+        static_max = .1
+        static_min = .001
+        nn.init.uniform_(self.wstatic.bias)
+        self.wstatic.bias.data = (self.wstatic.bias.data * (math.log(static_max) - math.log(static_min)) + math.log(static_min)).neg()
         # nn.init.trunc_normal_(self.sinks, mean=0.0, std=0.02)
         # self.gn.reset_parameters()
 
@@ -277,13 +284,21 @@ class Attention(nn.Module):
         output = [] #torch.zeros_like(xv)  # b h l d
         denom = []
         mask = torch.ones(c,l,device=xq.device,dtype=torch.bool)
+        static = nn.functional.softplus(self.wstatic(x)).neg().view(bs, seqlen, 2, self.n_kv_heads).sigmoid().permute(2,0,3,1)  # 2 b h l
+        static_src = static[0].unsqueeze(2)  # b h 1 l
+        static_dest = static[1].view(b, self.n_kv_heads, n, c)  # b h n c
 
         for i in range(n):
             k_ = kc[:,:,i]  # b h c d
             kt = xk.transpose(-2,-1)  # b h d l
             # Calculate decay
-            affinity = k_.matmul(kt).relu().to(dtype=torch.float).clamp(min=0,max=1).pow(2)
-            affinity = torch.log1p(affinity.neg().add(1e-6)).triu(i*c+1)  # b h c l
+            affinity = k_.matmul(kt).relu().to(dtype=torch.float).clamp(min=1e-12, max=1).log().mul(2)  # forget value: b h c l
+            # Calculate statics
+            static_dest_ = static_dest[:,:,i].unsqueeze(-1)  # b h c 1
+            # Calculate affinity, with low-skewed outliers
+            affinity = (affinity + static_src + static_dest).div(3).exp()
+            
+            affinity = torch.log1p(affinity.clamp(min=0,max=1-1e-12).neg()).triu(i*c+1)  # b h c l
             affinity = affinity.cumsum(3) #.exp().triu(i*c).unsqueeze(2).clamp(min=1e-12)  # b h 1 c l
             affinity = affinity.masked_fill(mask.tril(i*c-1), -1e12).unsqueeze(2)
             # Calculate attn scores
