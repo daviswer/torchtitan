599181b7f177e7f03b61159f5820fbb89932ff24
Davis Wertheimer <dww78@cornell.edu>
2025-03-22 20:53:58 -0400

    Prerelu static, then a la orig

--- torchtitan/models/llama/model.py
+++ torchtitan/models/llama/model.py
@@ -199,14 +199,14 @@ class Attention(nn.Module):
         self.wo = nn.Linear(
             model_args.n_heads * self.head_dim, model_args.dim, bias=False
         )
-        # self.wstatic = nn.Linear(model_args.dim, 2*self.n_kv_heads, bias=False)
+        self.wstatic = nn.Linear(model_args.dim, 2*self.n_kv_heads, bias=False)
         # self.gn = build_norm(
         #     model_args.norm_type, dim=self.head_dim, eps=model_args.norm_eps
         # )
         # self.sinks = nn.Parameter(torch.empty(2, self.n_kv_heads, self.head_dim, self.head_dim))
 
     def init_weights(self, init_std: float):
-        for linear in (self.wq, self.wk, self.wv):  #, self.wstatic):
+        for linear in (self.wq, self.wk, self.wv, self.wstatic):
             nn.init.trunc_normal_(linear.weight, mean=0.0, std=0.02)
         nn.init.trunc_normal_(self.wo.weight, mean=0.0, std=init_std)
         # static_max = .1
@@ -241,6 +241,10 @@ class Attention(nn.Module):
         xq = xq.view(bs, seqlen, -1, self.head_dim)
         xk = xk.view(bs, seqlen, -1, self.head_dim)
         xv = xv.view(bs, seqlen, -1, self.head_dim)
+        
+        # Normalize k
+        xk = xk/xk.pow(2).sum(-1, True).sqrt().add(1e-6)
+        # sinks = self.sinks/self.sinks.pow(2).sum(-1, True).sqrt().add(1e-6)
 
         # Compute sink scores before rope
         # sinks = sinks.repeat(1,self.n_rep,1,1)  # (k/v, h, d, d)
@@ -250,10 +254,6 @@ class Attention(nn.Module):
 
         # apply rope
         xq, xk = apply_rotary_emb(xq, xk, freqs_cis=freqs_cis)
-        
-        # Normalize k
-        xkr = xk/xk.pow(2).sum(-1, True).sqrt().add(1e-6)
-        # sinks = self.sinks/self.sinks.pow(2).sum(-1, True).sqrt().add(1e-6)
 
         # repeat k/v heads if n_kv_heads < n_heads
         # keys = repeat_kv(xk, self.n_rep)  # (bs, seqlen, n_local_heads, head_dim)
@@ -261,7 +261,6 @@ class Attention(nn.Module):
 
         xq = xq.transpose(1, 2)  # (bs, n_heads, seqlen, head_dim)
         xk = xk.transpose(1, 2)  # (bs, n_local_heads, seqlen, head_dim)
-        xkr = xkr.transpose(1, 2)  # (bs, n_local_heads, seqlen, head_dim)
         xv = xv.transpose(1, 2)  # (bs, n_local_heads, seqlen, head_dim)
         
         # Split out q if n_kv_heads < n_heads
@@ -285,22 +284,22 @@ class Attention(nn.Module):
         output = [] #torch.zeros_like(xv)  # b h l d
         denom = []
         mask = torch.ones(c,l,device=xq.device,dtype=torch.bool)
-        # static = self.wstatic(x).view(bs, seqlen, 2, self.n_kv_heads).permute(2,0,3,1)  # 2 b h l
-        # static_src = static[0].unsqueeze(2)  # b h 1 l
-        # static_dest = static[1].view(b, self.n_kv_heads, n, c)  # b h n c
+        static = self.wstatic(x).tanh().view(bs, seqlen, 2, self.n_kv_heads).permute(2,0,3,1)  # 2 b h l
+        static_src = static[0].unsqueeze(2)  # b h 1 l
+        static_dest = static[1].view(b, self.n_kv_heads, n, c)  # b h n c
 
         for i in range(n):
             k_ = kcr[:,:,i]  # b h c d
             kt = xk.transpose(-2,-1)  # b h d l
             # Calculate decay
-            affinity = k_.matmul(kt).float()  # forget value: b h c l
-            # # Calculate statics
-            # static_dest_ = static_dest[:,:,i].unsqueeze(-1)  # b h c 1
-            # # Calculate affinity, with low-skewed outliers
-            # affinity = (affinity + static_src + static_dest_).div(3).relu().neg().triu(i*c+1)  # b h c l
-            affinity = affinity.relu().neg().triu(i*c+1)  # b h c l
+            affinity = k_.matmul(kt)  # forget value: b h c l
+            # Calculate statics
+            static_dest_ = static_dest[:,:,i].unsqueeze(-1)  # b h c 1
+            # Calculate affinity, with low-skewed outliers
+            affinity = (affinity + static_src + static_dest_).div(3)
+            affinity = affinity.relu().clamp(min=0,max=1-1e-6).float().pow(2)
             
-            # affinity = torch.log1p(affinity.clamp(min=0,max=1-1e-6).neg()).triu(i*c+1)  # b h c l
+            affinity = torch.log1p(affinity.neg()).triu(i*c+1)  # b h c l
             affinity = affinity.cumsum(3) #.exp().triu(i*c).unsqueeze(2).clamp(min=1e-12)  # b h 1 c l
             affinity = affinity.masked_fill(mask.tril(i*c-1), -1e12).unsqueeze(2)
             # Calculate attn scores
