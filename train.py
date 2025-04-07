07901303dd33df68a0f880fe1afbc8d71cd46860
Davis Wertheimer <dww78@cornell.edu>
2025-04-07 17:13:46 -0400

    Resig

--- train.py
+++ train.py
@@ -275,7 +275,7 @@ def main(job_config: JobConfig):
     checkpoint.reset()
 
     with torch.no_grad():
-        inp = torch.arange(4096).view(1,8).cuda()
+        inp = torch.arange(512).view(1,512).cuda()
         pred = model(inp)[0,-8:]
     if torch.distributed.get_rank() == 0:
         print("Start signature:", pred.argmax(dim=-1))
@@ -457,7 +457,7 @@ def main(job_config: JobConfig):
                     world_mesh=world_mesh,
                 )
     with torch.no_grad():
-        inp = torch.arange(4096).view(1,8).cuda()
+        inp = torch.arange(512).view(1,512).cuda()
         pred = model(inp)[0,-8:]
     if torch.distributed.get_rank() == 0:
         print("End signature:", pred.argmax(dim=-1))
