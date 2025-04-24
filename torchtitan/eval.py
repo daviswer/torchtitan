aa74990e57fabbfe32b72271d56409968ad0184a
Davis Wertheimer <dww78@cornell.edu>
2025-04-24 15:26:29 -0400

    Add eval script

--- torchtitan/eval.py
+++ torchtitan/eval.py
@@ -1,12 +1,18 @@
+import argparse
 import os
 from typing import List, Optional, Union, Tuple
 
 import torch
 import torch.nn.functional as F
+import lm_eval
+from lm_eval.utils import make_table
 from lm_eval.api.instance import Instance  # type: ignore
 from lm_eval.api.model import LM  # type: ignore
 from lm_eval.api.registry import register_model  # type: ignore
 from torch import nn
+from torch import distributed as dist
+
+from torchtitan.models import model_name_to_cls, model_name_to_tokenizer, models_config
 
 def has_package(name):
     """
@@ -203,7 +209,7 @@ class FMSEvalHarnessLM(LM):
     def __init__(
         self,
         model: nn.Module,
-        tokenizer: tokenizers.BaseTokenizer,
+        tokenizer: BaseTokenizer,
         device="cpu",
         rank=0,
         world_size=1,
@@ -260,3 +266,131 @@ class FMSEvalHarnessLM(LM):
 
     def generate_until(self, requests: List[Instance]) -> List[str]:
         raise NotImplementedError("not implemented yet")
+
+
+# SCRIPT
+
+parser = argparse.ArgumentParser(description="Script to evaluate a causal model")
+parser.add_argument("--device_type", type=str, default="cuda")
+parser.add_argument(
+    "--architecture",
+    type=str,
+    default="llama",
+    help="The model architecture to benchmark",
+)
+parser.add_argument(
+    "--model_path",
+    type=str,
+    help="Path to the directory containing LLaMa weights (.pth files sharded by tensor parallel rank, not HF weights)",
+)
+parser.add_argument(
+    "--tokenizer",
+    type=str,
+    required=True,
+    help="Path to the tokenizer (e.g. ~/tokenizer.model)",
+)
+parser.add_argument(
+    "--no_use_cache",
+    action="store_false",
+    help="Disable the kv-cache (on by default)",
+)
+# parser.add_argument(
+#     "--compile",
+#     action="store_true",
+#     help="Use torch.compile (slow for first inference pass)",
+# )
+# parser.add_argument(
+#     "--compile_mode",
+#     type=str,
+#     help="Mode for compilation",
+#     default="default",
+#     choices=["default", "reduce-overhead"],
+# )
+parser.add_argument(
+    "--deterministic",
+    action="store_true",
+    help="Set torch.use_deterministic_algorithms? Requires env variable `CUBLAS_WORKSPACE_CONFIG=:4096:8`",
+)
+# parser.add_argument(
+#     "--distributed",
+#     action="store_true",
+#     help="This is a distributed job (multiple instances run with RANK+WORLD_SIZE)",
+# )
+parser.add_argument("--tasks", type=str, help="Task names to pass to lm_eval")
+parser.add_argument(
+    "--num_fewshot",
+    type=int,
+    default=None,
+    help="Number of examples in few-shot context",
+)
+
+args = parser.parse_args()
+
+local_rank = int(os.getenv("LOCAL_RANK", 0))
+world_size = int(os.getenv("WORLD_SIZE", 1))
+if args.device_type == "cuda":
+    device = torch.device(args.device_type, local_rank)
+    torch.cuda.set_device(device)
+else:
+    device = torch.device(args.device_type)
+
+torch.set_default_dtype(torch.half)
+
+# requires setting environment variable: `CUBLAS_WORKSPACE_CONFIG=:4096:8`
+if args.deterministic:
+    torch.use_deterministic_algorithms(True)
+
+# if args.distributed:
+#     dist.init_process_group()
+#     # Fix until PT 2.3
+#     torch._C._distributed_c10d._register_process_group("default", dist.group.WORLD)
+
+print("loading model")
+# if args.distributed:
+#     distr_param = "tp"
+# else:
+#     if torch.cuda.device_count() > 1 and world_size == 1:
+#         distr_param = "mp"
+#     else:
+#         distr_param = None
+
+# model = get_model(
+#     args.architecture,
+#     args.variant,
+#     model_path=args.model_path,
+#     device_type=args.device_type,
+#     source=args.model_source,
+#     distributed_strategy=distr_param,
+#     group=dist.group.WORLD,
+# )
+model_name = "llama3"
+model_cls = model_name_to_cls[model_name] # TODO import
+model_config = models_config[model_name][args.architecture] # TODO import
+model_config.vocab_size = 128256
+model_config.max_seq_len = 4096
+model = model_cls.from_model_args(model_config)
+
+tokenizer = get_tokenizer(args.tokenizer)
+model.eval()
+torch.set_grad_enabled(False)
+print("loading complete on rank", local_rank)
+
+# if args.compile:
+#     print("compiling model")
+#     # Bug with kv-cache in PT2.1
+#     torch._inductor.config.joint_graph_constant_folding = False
+#     # compiling can make first inference pass slow
+#     model = torch.compile(model, mode=args.compile_mode)
+
+
+lm_obj = FMSEvalHarnessLM(model=model, tokenizer=tokenizer, device=device)
+
+results = lm_eval.simple_evaluate(
+    model=lm_obj,
+    tasks=args.tasks.split(","),
+    num_fewshot=args.num_fewshot,
+)
+print(make_table(results))
+if "groups" in results:
+    print(make_table(results, "groups"))
+
