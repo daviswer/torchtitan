e55adf4e3426ad61edb42c3a96d2004739574835
Davis Wertheimer <dww78@cornell.edu>
2025-04-25 15:37:32 -0400

    cudafy

--- eval.py
+++ eval.py
@@ -371,6 +371,7 @@ model_config = models_config[model_name][args.architecture] # TODO import
 model_config.vocab_size = 128256
 model_config.max_seq_len = 4096
 model = model_cls.from_model_args(model_config)
+model.cuda()
 model.init_weights()
 
 # load state dict
