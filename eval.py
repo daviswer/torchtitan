f48665193669cff1dace0edca40c86d2ac8b7ae9
Davis Wertheimer <dww78@cornell.edu>
2025-04-25 15:49:39 -0400

    Cuda after load

--- eval.py
+++ eval.py
@@ -371,8 +371,7 @@ model_config = models_config[model_name][args.architecture] # TODO import
 model_config.vocab_size = 128256
 model_config.max_seq_len = 4096
 model = model_cls.from_model_args(model_config)
-model.cuda()
-model.init_weights()
+# model.init_weights()
 
 # load state dict
 # state_dict = {"model": model.state_dict()}
@@ -383,6 +382,7 @@ model.init_weights()
 # model.load_state_dict(state_dict["model_state"])
 state_dict = load(keys={"model":None}, checkpoint_id=args.model_path)
 model.load_state_dict(state_dict["model"])
+model.cuda()
 
 tokenizer = get_tokenizer(args.tokenizer)
 model.eval()
