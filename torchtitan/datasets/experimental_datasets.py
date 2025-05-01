cf6f540f59a29b8304ef0f446c02cab36c1d76cc
Davis Wertheimer <dww78@cornell.edu>
2025-05-01 16:43:30 -0400

    Remove unnecessary ifbranch

--- torchtitan/datasets/experimental_datasets.py
+++ torchtitan/datasets/experimental_datasets.py
@@ -1020,9 +1020,8 @@ class StreamingDocDataset(_StatefulDataset):
                 ndocs = doc_counts[shard]
                 doc_start = int(ndocs * shardset[shard][0])
                 doc_end = max(doc_start, int(ndocs * shardset[shard][1]) - 1)  # inclusive upper bound
-                if doc_end >= doc_start:
-                    self.docset.append([shard, doc_start, doc_end])
-                    doccount += doc_end - doc_start + 1
+                self.docset.append([shard, doc_start, doc_end])
+                doccount += doc_end - doc_start + 1
             self._len = doccount
 
             if self.verbose:
