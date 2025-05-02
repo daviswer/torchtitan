188b8bdc58c0514657829b698ab63ab3ac53a562
Davis Wertheimer <dww78@cornell.edu>
2025-05-01 20:35:31 -0400

    Check for nonexistent paths

--- torchtitan/datasets/experimental_datasets.py
+++ torchtitan/datasets/experimental_datasets.py
@@ -1366,6 +1366,8 @@ class SamplingDataset(_WrapperDataset):
             ]
         )
         assert len(self.datasets) > 0, "You must specify at least one dataset"
+        for d in datasets:
+            assert os.path.exists(os.path.join(datapath, d)), f"Invalid subdataset path: {os.path.exists(os.path.join(datapath, d))}"
 
         if weights is not None:
             assert len(weights) == len(
