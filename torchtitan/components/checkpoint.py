0f04abd644769f68fd53ea344e68e26567297f28
Davis Wertheimer <davis.wertheimer@ibm.com>
2025-11-25 12:21:26 -0500

    diag print

--- torchtitan/components/checkpoint.py
+++ torchtitan/components/checkpoint.py
@@ -775,6 +775,9 @@ class CheckpointManager:
             )
         else:
             logger.info(f"Saving a full checkpoint at last step, step {curr_step}.")
+            print(states.keys())
+            time.sleep(1)
+            print(states)
             states = self._flattened_model_states_sd()
 
         if self.last_save_in_hf:
--- torchtitan/models/llama3/__init__.py
+++ torchtitan/models/llama3/__init__.py
@@ -70,7 +70,6 @@ llama3_args = {
 
 
 def get_train_spec() -> TrainSpec:
-    print(".   GOT INTO LLAMA3 TRAIN SPEC")
     return TrainSpec(
         model_cls=Transformer,
         model_args=llama3_args,
