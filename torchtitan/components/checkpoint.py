2e41c4a31d8193a537730ffd6b415dd688fed31a
lchu <lchu@us.ibm.com>
2025-12-13 19:18:18 +0000

    add support for warm-start from a ckpt that saved from an older version of torchtitan

--- torchtitan/components/checkpoint.py
+++ torchtitan/components/checkpoint.py
@@ -477,7 +477,9 @@ class CheckpointManager:
             # TODO: Since we flatten the model states in state_dict, we need to
             # manually call load_state_dict() for the model. Need to fix this.
             if MODEL in self.states:
-                self.states[MODEL].load_state_dict(state_dict)
+                # self.states[MODEL].load_state_dict(state_dict)
+                # for ckpt saved with older torchtitan versions (non-flattened ckpt), use the following:
+                self.states[MODEL].load_state_dict(state_dict[MODEL])
         
         # Handle rescaling dataloader separately
         if self.rescaling_mesh is not None and not model_only:
@@ -761,6 +763,8 @@ class CheckpointManager:
         """
         # For the first step, we will only load the model.
         if model_only:
+            # return self.states[MODEL].state_dict()
+            # for ckpt saved with older torchtitan versions (non-flattened ckpt), use the following:
             return {MODEL: self.states[MODEL].state_dict()}
 
         for exclude_key in self.exclude_from_loading:
