0a891b85322af8e0303a280fe5997027b08ff7c6
Davis Wertheimer <davis.wertheimer@ibm.com>
2026-04-10 01:18:18 -0400

    Grow window to test yielding

--- torchtitan/experiments/vlm/datasets/mm_datasets.py
+++ torchtitan/experiments/vlm/datasets/mm_datasets.py
@@ -493,7 +493,7 @@ def build_mm_dataloader(
 
     dataset = DictShuffleDataset(
         dataset,
-        window_size = 2,
+        window_size = 4,
         seed = 42,
         n_data_fields = 4,
     )
