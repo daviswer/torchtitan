62d4ddc76c3e7f8ea0c45f1724f3556ed5268783
Davis Wertheimer <davis.wertheimer@ibm.com>
2026-04-10 15:22:26 -0400

    Extra diag print for r6

--- torchtitan/experiments/vlm/datasets/mm_datasets.py
+++ torchtitan/experiments/vlm/datasets/mm_datasets.py
@@ -199,8 +199,8 @@ MM_DATASETS = {
         sample_processor=_process_obelics_sample,
     ),
     "cc12m": DatasetConfig(
-        path="pixparse/cc12m-wds",
-        # path="/gpfs/davis/temp_data/cc12m/datasets--pixparse--cc12m-wds/snapshots/796118f2eabdb9984f23f7f15d1e74d388612fc6",
+        # path="pixparse/cc12m-wds",
+        path="/gpfs/davis/temp_data/cc12m/datasets--pixparse--cc12m-wds/snapshots/796118f2eabdb9984f23f7f15d1e74d388612fc6",
         loader=lambda path: load_dataset(path, split="train", streaming=True),
         sample_processor=_process_cc12_wd_sample,
     ),
@@ -506,6 +506,15 @@ def build_mm_dataloader(
     dataset = PreprocessDataset(
         dataset,
         labelput,
+    )
+
+    def diag(x):
+        if dp_rank==6:
+            print(x)
+        return x
+    dataset = PreprocessDataset(
+        dataset,
+        diag,
     )
 
     base_dataloader = StatefulDataLoader(
