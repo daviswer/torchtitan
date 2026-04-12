b4d015e95aefad3ea31ef191f8df9bfcd567c39a
Davis Wertheimer <davis.wertheimer@ibm.com>
2026-04-12 01:33:12 -0400

    Window size 20

--- torchtitan/experiments/vlm/datasets/mm_datasets.py
+++ torchtitan/experiments/vlm/datasets/mm_datasets.py
@@ -493,7 +493,7 @@ def build_mm_dataloader(
 
     dataset = DictShuffleDataset(
         dataset,
-        window_size = 100,
+        window_size = 20,
         seed = 42,
         n_data_fields = 4,
     )
